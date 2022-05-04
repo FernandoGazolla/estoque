e8d1390a89fa385b2ea87fef93456a3f0770bc9a
Fernando Gazolla <30941854+FernandoGazolla@users.noreply.github.com>
2022-05-04 20:09:10 -0300

    Atualização de Produtos

--- mobile/view/Produto.jsx
+++ mobile/view/Produto.jsx
@@ -5,6 +5,7 @@ import {View,
     Button} from 'react-native';
 
 import { useState } from 'react';
+import {Picker} from '@react-native-picker/picker'
 
 export default function Produto()
 {
@@ -13,6 +14,16 @@ export default function Produto()
     const [codBarras, setCodBarras] = useState('');
     const [localidade, setLocalidade] = useState('');
     const [geoLoc, setGeoLoc] = useState('');
+
+    const [locCadastradas, setLocCdadastradas] = useState([
+        {nome: "Filial São Paulo", id: "filial-SP"},
+        {nome: "Filial Cuiaba", id: "filial-MT"},
+        {nome: "Filial Minas Gerais", id: "filial-MG"}
+    ]);
+
+    const itens = locCadastradas.map(function (Item){
+        return <Picker.Item label= {Item.nome} value= {Item.id}></Picker.Item>
+    });
 
     return (
         <View>
@@ -29,6 +40,9 @@ export default function Produto()
                 onChangeText={(valor) => setLocalidade(valor)}
                 style={css.input} 
                 placeholder='Localidade'/>
+                <Picker onValueChange={(valor)=> setLocalidade(valor)}>
+                    {itens}
+                </Picker>
             <TextInput 
                 onChangeText={(valor) => setGeoLoc(valor)}
                 style={css.input} 
